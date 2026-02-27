f757598422f2055a83867ac31908dfe81f1ac35a
Frostwolf74 <152447084+Frostwolf74@users.noreply.github.com>
2026-02-26 20:41:44 -0700

    changed icons

--- Assignment2/mobile-ui/components/HomePosts.tsx
+++ Assignment2/mobile-ui/components/HomePosts.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from "react-native";
 import React, { use } from "react";
 
 const HomePosts = () => {
@@ -71,7 +71,15 @@ const HomePosts = () => {
       {/* Interaction Bar */}
       <View style={styles.interactionBar}>
         <View style={styles.interactionItem}>
-          <Text style={styles.interactionItemIcon}>❤️</Text>
+          <button style={styles.interactionItemIcon}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+            </svg>
+             {/* heart should become filled when clicked -> 
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                </svg> */}
+          </button>
           <Text style={styles.interactionItemText}>16.2K</Text>
         </View>
 
@@ -81,7 +89,15 @@ const HomePosts = () => {
         </View>
 
         <View style={styles.interactionItem}>
-          <Text style={styles.interactionItemIcon}>↗️</Text>
+          <button style={styles.interactionItemIcon}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+            </svg>
+            {/* filled version ->
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+              </svg> */}
+          </button>
           <Text style={styles.interactionItemText}>146</Text>
         </View>
 
@@ -174,6 +190,8 @@ const styles = StyleSheet.create({
   },
   interactionItemIcon: {
     fontSize: 18,
+    borderColor: "transparent",
+    backgroundColor: "transparent"
   },
   interactionItemText: {
     color: "#0e0d0d",
